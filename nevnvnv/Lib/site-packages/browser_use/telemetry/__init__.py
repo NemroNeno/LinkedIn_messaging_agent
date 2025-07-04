3433080b33a163ce522af538254da2472f368625
nemro <mnabeel.bese22seecs.seecs.edu.pk>
2025-07-04 23:23:24 +0500

    ignored folder

--- nevnvnv/Lib/site-packages/browser_use/telemetry/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Telemetry for Browser Use.
-"""
-
-from browser_use.telemetry.service import ProductTelemetry
-from browser_use.telemetry.views import BaseTelemetryEvent
-
-__all__ = ['BaseTelemetryEvent', 'ProductTelemetry']
